911b9d6c7d15a2fabaf9acdd39254de5c1258fbd
quangnguyen11 <dquang888@gmail.com>
2018-11-20 14:43:32 +0700

    github
    github

--- CloudScript.js
+++ CloudScript.js
@@ -38,7 +38,7 @@ handlers.helloWorld = function (args, context) {
     
     // The pre-defined "currentPlayerId" variable is initialized to the PlayFab ID of the player logged-in on the game client. 
     // Cloud Script handles authenticating the player automatically.
-    var message = "Hello " + currentPlayerId + "!";
+    var message = "Hello from Github" + currentPlayerId + "!";
 
     // You can use the "log" object to write out debugging statements. It has
     // three functions corresponding to logging level: debug, info, and error. These functions
